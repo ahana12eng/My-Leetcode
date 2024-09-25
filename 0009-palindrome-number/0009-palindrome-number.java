6924aa768e7b424e75933a2b5acc07f516ba4016
AHANA DUTTA <duttaahana70@gmail.com>
2024-09-26 00:19:20 +0530

    Time: 5 ms (71.12%), Space: 43.9 MB (64.08%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        int temp=x;
+        int sum=0;
+        while(x!=0){
+                int rem=x%10;
+                sum=sum*10+rem;
+                x/=10;
+        }
+
+        if(sum==temp){
+                return true;
+        }
+
+        return false;
+    }
+}
