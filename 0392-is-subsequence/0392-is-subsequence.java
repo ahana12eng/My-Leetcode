5d1fa03f7b46c498e8888552823381c97f199c42
AHANA DUTTA <duttaahana70@gmail.com>
2024-09-26 09:31:29 +0530

    Time: 2 ms (60.06%), Space: 41.4 MB (39.92%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        
+
+         if(s.length()==0){
+            return true;
+        }
+        
+       
+        int i=0;
+        for(int j=0;j<t.length();j++){
+            if(s.charAt(i)==t.charAt(j)){
+              
+                i++;
+              
+            }
+
+            if(s.length()==i){
+                return true;
+            }
+           
+        }
+     return i==s.length();
+    }
+}
